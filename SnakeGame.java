1b30ee71cfb7842ce87d2189f5fab5d23e66cc45
Toby Nguyen <tobyanguyen1@gmail.com>
2024-03-18 16:32:06 -0500

    pushing finished content

---
+++ SnakeGame.java
@@ -0,0 +1,15 @@
+package SnakeStuff;
+
+public class SnakeGame {
+    
+    /** 
+     * 
+     * this is a main method
+     * 
+     * @param args
+     */
+    public static void main(String[] args) {
+        
+        new GameFrame();
+    }
+}
